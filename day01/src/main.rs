c71ba4b7a76ad75b3099c9a49d763da272b22c4c
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-09 19:19:05 +0000

    day01 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -12,6 +12,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|l| l.unwrap().parse().unwrap())
         .collect();
 
+    // part 1
     for i in 0..list.len() - 1 {
         for j in i + 1..list.len() {
             let vi = list[i];
@@ -19,12 +20,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             if vi + vj == 2020 {
                 println!(
-                    "vi = {}, vj = {}, vi + vj = {}, vi * vj = {}",
+                    "part1 -> vi = {}, vj = {}, vi + vj = {}, vi * vj = {}",
                     vi,
                     vj,
                     vi + vj,
                     vi * vj
                 );
+            }
+        }
+    }
+
+    // part 2
+    for i in 0..list.len() - 2 {
+        for j in i + 1..list.len() - 1 {
+            for k in j + 1..list.len() {
+                let vi = list[i];
+                let vj = list[j];
+                let vk = list[k];
+
+                if vi + vj + vk == 2020 {
+                    println!(
+                        "part2 -> vi = {}, vj = {}, vk = {}, sum = {}, product = {}",
+                        vi,
+                        vj,
+                        vk,
+                        vi + vj + vk,
+                        vi * vj * vk
+                    );
+                }
             }
         }
     }
