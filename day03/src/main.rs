91a95396d685d404208702e02e890ecf18684d7a
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-09 20:14:58 +0000

    day03 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -14,27 +14,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // part 1
     {
-        let mut x = 0_usize;
-        let mut y = 0_usize;
-
-        let dx = 3_usize;
-        let dy = 1_usize;
-
-        let mut trees = 0;
-        loop {
-            x += dx;
-            y += dy;
-
-            if y >= map.len() {
-                break;
-            }
-
-            if encountered_tree(&map, x, y) {
-                trees += 1;
-            }
-        }
-
+        let trees = count_encountered_trees(&map, 3, 1);
         println!("Part1 -> trees = {}", trees);
+    }
+
+    // part 2
+    {
+        let paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+        let counts = paths
+            .iter()
+            .map(|(dx, dy)| count_encountered_trees(&map, *dx, *dy));
+        let product:i64 = counts.map(|v| v as i64).product(); // overflows with i32
+        println!("Part2 -> product = {}", product);
     }
 
     Ok(())
@@ -54,4 +45,23 @@ fn encountered_tree(map: &Vec<Vec<bool>>, x: usize, y: usize) -> bool {
     let row = &map[y];
     let x_mod = x % row.len();
     row[x_mod]
+}
+
+fn count_encountered_trees(map: &Vec<Vec<bool>>, dx: usize, dy: usize) -> i32 {
+    let mut x = 0_usize;
+    let mut y = 0_usize;
+    let mut trees = 0;
+    loop {
+        x += dx;
+        y += dy;
+
+        if y >= map.len() {
+            break;
+        }
+
+        if encountered_tree(&map, x, y) {
+            trees += 1;
+        }
+    }
+    trees
 }
