ddd6a1979568cd1885c02353dea0bb1c29cf0c91
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-09 20:06:11 +0000

    day03 part 1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use core::panic;
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("input.txt")?;
+    let buffered = BufReader::new(file);
+
+    let map: Vec<Vec<bool>> = buffered.lines().map(|l| parse_line(&l.unwrap())).collect();
+    //println!("{:?}", map);
+
+    // part 1
+    {
+        let mut x = 0_usize;
+        let mut y = 0_usize;
+
+        let dx = 3_usize;
+        let dy = 1_usize;
+
+        let mut trees = 0;
+        loop {
+            x += dx;
+            y += dy;
+
+            if y >= map.len() {
+                break;
+            }
+
+            if encountered_tree(&map, x, y) {
+                trees += 1;
+            }
+        }
+
+        println!("Part1 -> trees = {}", trees);
+    }
+
+    Ok(())
+}
+
+fn parse_line(line: &str) -> Vec<bool> {
+    line.chars()
+        .map(|ch| match ch {
+            '.' => false,
+            '#' => true,
+            _ => panic!("unexpected char"),
+        })
+        .collect()
+}
+
+fn encountered_tree(map: &Vec<Vec<bool>>, x: usize, y: usize) -> bool {
+    let row = &map[y];
+    let x_mod = x % row.len();
+    row[x_mod]
 }
