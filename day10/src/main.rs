e0caee758393b19edd0c05ce7bd56ab04b9f3808
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-14 18:56:53 +0000

    part2 needs work to reduce problem size

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -23,6 +23,7 @@ impl AdaptersProblem {
         self.find_chain_internal(vec![0], bag)
     }
 
+    // need to use all the adapters
     fn find_chain_internal(&self, chain: Vec<i32>, bag: HashSet<i32>) -> Option<Vec<i32>> {
         if bag.is_empty() {
             let mut new_chain = chain.clone();
@@ -51,6 +52,41 @@ impl AdaptersProblem {
             chain_option
         }
     }
+
+    fn count_chains(&self) -> i32 {
+        let bag: HashSet<i32> = self.adapters.iter().copied().collect();
+        self.count_chains_internal(vec![0], bag)
+    }
+
+    // need to use the adapters to reach the target voltage; not necessary to use all of them
+    fn count_chains_internal(&self, chain: Vec<i32>, bag: HashSet<i32>) -> i32 {
+        let last = *chain.last().unwrap();
+        if last+3 == self.final_joltage {
+            // we've hit our target; this chain terminates valid
+            1
+        } else {
+            // find all adapters that can connect to the end of the existing chain
+            let mut viable_adapters: Vec<_> = bag
+                .iter()
+                .filter(|&&adapt| {
+                    let diff = adapt - last;
+                    diff >= 1 && diff <= 3
+                })
+                .collect();
+            // now sort, and pick the smallest item first (greedy)
+            viable_adapters.sort();
+            // recursively consider options
+            let valid_chains = viable_adapters.iter().map(|&adapt| {
+                let mut new_chain = chain.clone();
+                let mut new_bag = bag.clone();
+                new_chain.push(*adapt);
+                new_bag.remove(adapt);
+                self.count_chains_internal(new_chain, new_bag)
+            });
+            // return sum of all valid chains (will be 0 if there are none)
+            valid_chains.sum()
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -60,6 +96,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         let joltage: i32 = l?.parse()?;
         adapters.push(joltage);
     }
+
+    println!("Part 1 ----");
 
     let problem = AdaptersProblem::create(adapters);
     let chain = problem.find_chain().unwrap();
@@ -80,6 +118,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         diffs_3,
         diffs_1 * diffs_3
     );
+
+    println!("Part 2 ----");
+    let counted = problem.count_chains();
+    println!("Possible chains: {}", counted);
 
     Ok(())
 }
