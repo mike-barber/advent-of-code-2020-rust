50d53ecb87cbf8efce9382dde81a73f83cb1ebfc
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-16 23:56:31 +0000

    day15 cleanup using scan iterator; somewhat more composable

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,30 +1,4 @@
-use std::{
-    collections::{HashMap, VecDeque},
-    hash::Hash,
-    mem, todo,
-};
-
-struct RecentNumbers(usize, VecDeque<i32>);
-impl RecentNumbers {
-    fn create_with(capacity: usize, value: i32) -> Self {
-        let mut vec = VecDeque::new();
-        vec.push_back(value);
-        Self(capacity, vec)
-    }
-
-    fn push(&mut self, value: i32) {
-        let vec = &mut self.1;
-        vec.push_back(value);
-        if vec.len() > self.0 {
-            vec.pop_front();
-        }
-    }
-
-    // just return the iterator from the VecDeque
-    fn iter(&self) -> impl Iterator<Item = &i32> {
-        self.1.iter()
-    }
-}
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy)]
 struct NumberSpoken {
@@ -32,44 +6,74 @@ struct NumberSpoken {
     prior_round: Option<i32>,
 }
 
-fn main() {
-    //let starting_numbers = [0, 3, 6];
-    let starting_numbers = [16,11,15,0,1,7];
-    //let rounds = 2020i32;
-    let rounds = 30000000;
+fn run_game(round: i32, starting_numbers: &[i32]) -> impl Iterator<Item = i32> {
+    struct State {
+        memory: HashMap<i32, NumberSpoken>,
+        last_number_spoken: i32,
+    };
 
-    let mut memory: HashMap<i32, NumberSpoken> = HashMap::new();
-    let mut last_number_spoken = 0;
-
-    for round in 1..=rounds {
-        let num = {
-            if round <= starting_numbers.len() as i32 {
-                let num = starting_numbers[round as usize - 1];
-                num
-            } else {
-                if let Some(m) = memory.get(&last_number_spoken) {
-                    if let Some(prior) = m.prior_round {
-                        m.round - prior
+    let starting_numbers = starting_numbers.to_vec();
+    let rounds = 1..=round;
+    rounds.scan(
+        State {
+            memory: HashMap::new(),
+            last_number_spoken: 0,
+        },
+        move |state, round| {
+            let num = {
+                if round <= starting_numbers.len() as i32 {
+                    let num = starting_numbers[round as usize - 1];
+                    num
+                } else {
+                    if let Some(m) = state.memory.get(&state.last_number_spoken) {
+                        if let Some(prior) = m.prior_round {
+                            m.round - prior
+                        } else {
+                            0
+                        }
                     } else {
                         0
                     }
-                } else {
-                    0
                 }
-            }
-        };
-        // update memory 
-        if let Some(m) = memory.get_mut(&num) {
-            *m = NumberSpoken { 
-                round,
-                prior_round: Some(m.round)
             };
-        } else {
-            memory.insert(num, NumberSpoken { round, prior_round: None });
-        }
-        // record last number spoken
-        last_number_spoken = num;
-        //println!("round {} -> num {}", round, num);
+            // update memory
+            if let Some(m) = state.memory.get_mut(&num) {
+                *m = NumberSpoken {
+                    round,
+                    prior_round: Some(m.round),
+                };
+            } else {
+                state.memory.insert(
+                    num,
+                    NumberSpoken {
+                        round,
+                        prior_round: None,
+                    },
+                );
+            }
+            // record last number spoken
+            state.last_number_spoken = num;
+            Some(num)
+        },
+    )
+}
+
+fn main() {
+    println!("Example ----");
+    {
+        let numbers: Vec<_> = run_game(10, &[0, 3, 6]).collect();
+        println!("{:?}", numbers);
     }
-    println!("last number: {}", last_number_spoken);
+
+    println!("Part 1 ----");
+    println!(
+        "Final number: {:?}",
+        run_game(2020, &[16, 11, 15, 0, 1, 7]).last()
+    );
+
+    println!("Part 2 ----");
+    println!(
+        "Final number: {:?}",
+        run_game(30000000, &[16, 11, 15, 0, 1, 7]).last()
+    );
 }
