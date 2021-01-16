a4f1eba35e0d2a1677815b1a99b1edb53faa0aac
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-16 13:17:23 +0000

    more messing around with part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -45,6 +45,50 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("-----");
     }
 
+    // Part 2
+    {
+        // get ids and offsets
+        let mut id_offset: Vec<_> = ids
+            .iter()
+            .enumerate()
+            .filter_map(|(idx, v)| v.map(|v| (v, idx as i64)))
+            .collect();
+        id_offset.sort_by_key(|(id, _offset)| *id);
+        id_offset.reverse();
+
+        // aligned vectors of ids and offsets
+        let effective_ids: Vec<i64> = id_offset.iter().map(|(id, _offset)| *id).collect();
+        let offsets: Vec<i64> = id_offset.iter().map(|(_id, offset)| *offset).collect();
+        // sort largest to smallest on id -- we'll use the largest ID as the stride
+        println!("ids:     {:?}", &effective_ids);
+        println!("offsets: {:?}", &offsets);
+
+        let stride = effective_ids[0];
+        let first_time = effective_ids[0] - offsets[0];
+        println!("first time: {}, stride: {}", first_time, stride);
+
+        // relying on the compiler to do some nice vectorisation here, rather than
+        // testing every bus.
+        let mut residuals = vec![0i64; effective_ids.len()];
+        let mut t_now = first_time;
+        loop {
+            residuals
+                .iter_mut()
+                .zip(&effective_ids)
+                .zip(&offsets)
+                .for_each(|((res, id), off)| {
+                    let t_bus = t_now + off;
+                    *res = t_bus % id;
+                });
+
+            if residuals.iter().all(|r| *r == 0) {
+                println!("Found time: {}", t_now);
+                break;
+            }
+            t_now += stride;
+        }
+    }
+
     // Part 2 - second attempt
     {
         let id_offset: Vec<_> = ids
@@ -60,10 +104,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .iter()
                 .map(|(id, offset)| {
                     let t = t_now + offset;
-                    (t / id + 1) * id
+                    let t_next = (t / id + 1) * id - offset;
+                    //println!("t_next {} for id {} offset {}", t_next, id, offset);
+                    t_next
                 })
-                .min()
+                .max()
                 .unwrap();
+
+            //println!("next: {} (delta +{})", t_next_possible, t_next_possible-t_now);
 
             // check if it matches all the busses
             t_now = t_next_possible;
@@ -74,40 +122,6 @@ fn main() -> Result<(), Box<dyn Error>> {
                 println!("Found time: {}", t_now);
                 break;
             }
-        }
-    }
-
-    // Part 2
-    {
-        // aligned vectors of ids and offsets
-        let effective_ids: Vec<i64> = ids.iter().filter_map(|x| *x).collect();
-        let offsets: Vec<i64> = ids
-            .iter()
-            .enumerate()
-            .filter_map(|(idx, b)| b.map(|_| idx as i64))
-            .collect();
-        println!("ids:     {:?}", &effective_ids);
-        println!("offsets: {:?}", &offsets);
-        let stride = effective_ids[0];
-
-        // relying on the compiler to do some nice vectorisation here, rather than
-        // testing every bus.
-        let mut residuals = vec![0i64; effective_ids.len()];
-        let mut t_now = 0;
-        loop {
-            residuals
-                .iter_mut()
-                .zip(&effective_ids)
-                .zip(&offsets)
-                .for_each(|((res, id), off)| {
-                    let t_bus = t_now + off;
-                    *res = t_bus % id;
-                });
-            if residuals.iter().all(|r| *r == 0) {
-                println!("Found time: {}", t_now);
-                break;
-            }
-            t_now += stride;
         }
     }
 
