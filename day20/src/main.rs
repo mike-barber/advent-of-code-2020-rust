c475ddbb671b9694a9dd27a8720693e253328d0a
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2021-01-22 09:36:24 +0000

    tile

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -14,46 +14,81 @@ impl Display for Coord {
     }
 }
 
+
 enum Rotation {
-    L0,
-    L90,
-    L180,
-    L270
+    R0,
+    R90,
+    R180,
+    R270
 }
 impl Rotation {
     fn ident(c:&Coord, _dim:i32) -> Coord {
         Coord(c.0, c.1)
     }
-    fn ccw90(c:&Coord, dim:i32) -> Coord {
+    fn cw90(c:&Coord, dim:i32) -> Coord {
         Coord(dim-1-c.1, c.0)
     }
     // TODO: direct matrices instead of nested calls
-    fn ccw180(c:&Coord, dim:i32) -> Coord {
-        Self::ccw90(&Self::ccw90(&c,dim),dim)
+    fn cw180(c:&Coord, dim:i32) -> Coord {
+        Self::cw90(&Self::cw90(&c,dim),dim)
     }
-    fn ccw270(c:&Coord, dim:i32) -> Coord {
-        Self::ccw90(&Self::ccw90(&Self::ccw90(&c,dim),dim),dim)
+    fn cw270(c:&Coord, dim:i32) -> Coord {
+        Self::cw90(&Self::cw90(&Self::cw90(&c,dim),dim),dim)
     }
     
     fn get_function(&self) -> fn(&Coord, i32) -> Coord {
         match self {
-            Rotation::L0 => Self::ident,
-            Rotation::L90 => Self::ccw90,
-            Rotation::L180 => Self::ccw180,
-            Rotation::L270 => Self::ccw270
+            Rotation::R0 => Self::ident,
+            Rotation::R90 => Self::cw90,
+            Rotation::R180 => Self::cw180,
+            Rotation::R270 => Self::cw270
         }
     }
 
     // get next rotation, but stop after 270 degrees 
     fn next_stop(&self) -> Option<Rotation> {
         match self {
-            Rotation::L0 => Some(Rotation::L90),
-            Rotation::L90 => Some(Rotation::L180),
-            Rotation::L180 => Some(Rotation::L270),
-            Rotation::L270 => None
+            Rotation::R0 => Some(Rotation::R90),
+            Rotation::R90 => Some(Rotation::R180),
+            Rotation::R180 => Some(Rotation::R270),
+            Rotation::R270 => None
         }
     }
+}
 
+struct Tile {
+    image: Array2<char>,
+    dim: i32
+}
+impl Tile {
+    fn get(&self, c: &Coord) -> char {
+        *self.image.get((c.0 as usize, c.1 as usize)).expect("dimension exceeded")
+    }
+}
+
+struct RotatedTile<'a> {
+    tile: &'a Tile,
+    rotation: Rotation
+}
+
+impl<'a> RotatedTile<'a> {
+    fn get(&self, c: &Coord) -> char {
+        let c = self.rotation.get_function()(&c, self.tile.dim);
+        self.tile.get(&c)
+    }
+}
+
+impl<'a> Display for RotatedTile<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for r in 0..self.tile.dim {
+            writeln!(f)?;
+            for c in 0..self.tile.dim {
+                let ch = self.get(&Coord(r,c));
+                write!(f, "{}", ch)?;
+            }
+        }
+        Ok(())
+    }
 }
 
 /// notes
@@ -77,10 +112,10 @@ fn main() -> Result<()> {
     let dim = 3;
     let c = Coord(1,2);
 
-    let f0 = Rotation::L0.get_function();
-    let f1 = Rotation::L90.get_function();
-    let f2 = Rotation::L180.get_function();
-    let f3 = Rotation::L270.get_function();
+    let f0 = Rotation::R0.get_function();
+    let f1 = Rotation::R90.get_function();
+    let f2 = Rotation::R180.get_function();
+    let f3 = Rotation::R270.get_function();
 
     let c0 = f0(&c,dim);
     let c1 = f1(&c,dim);
@@ -91,6 +126,22 @@ fn main() -> Result<()> {
     println!("{}",c1);
     println!("{}",c2);
     println!("{}",c3);
+
+
+    let tile = Tile {
+        dim: 3,
+        image: arr2(&[['1','2','3'],['4','5','6'],['7','8','9']])
+    };
+    let t0 = RotatedTile { tile: &tile, rotation: Rotation::R0 };
+    let t1 = RotatedTile { tile: &tile, rotation: Rotation::R90 };
+    let t2 = RotatedTile { tile: &tile, rotation: Rotation::R180 };
+    let t3 = RotatedTile { tile: &tile, rotation: Rotation::R270 };
+    println!("{}", &t0);
+    println!("{}", &t1);
+    println!("{}", &t2);
+    println!("{}", &t3);
+
+
 
     Ok(())
 }
